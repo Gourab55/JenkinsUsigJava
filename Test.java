5c3f3e5865f8f0ce99f3863e59dc64d28f164589
Gourab55 <rajgourab55@gmail.com>
2025-02-25 15:02:01 +0530

    test file added

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        
+    int n=5;
+    int res=1;
+    for(int i=1;i<=5;i++)
+     res= res*i;
+     System.out.println("the factoril of"+n+" is "+res);
+    }
+    
+}
